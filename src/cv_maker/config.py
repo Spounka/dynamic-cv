24f36553eb6cb00be767dc4500de1ce3c2e3b3ee
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2024-07-01 12:17:55 +0100

    fix:updated directories to match new structure

--- src/cv_maker/config.py
+++ src/cv_maker/config.py
@@ -3,10 +3,10 @@ from typing import List, Literal
 
 
 BASE_DIR = Path(__file__).parent.parent.parent.resolve()
-DATA_DIR = BASE_DIR / "data"
-TEMPLATES = BASE_DIR / "template"
+DATA_DIR = BASE_DIR / "src" / "data"
+TEMPLATES = BASE_DIR / "src" / "template"
 BUILD_DIR = BASE_DIR / "build"
-PATH_TO_IMAGE = BASE_DIR / "images" / "NazihPicture3.jpg"
+PATH_TO_IMAGE = BASE_DIR / "src" / "images" / "NazihPicture3.jpg"
 
 LANGS: List[Literal["en", "fr"]] = ["en", "fr"]
 FORMATS = ["ats", "pretty"]
