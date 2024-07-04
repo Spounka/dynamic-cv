f8666bce408ddd37ae030dfdc0a32167fce98fa1
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2024-07-05 00:00:20 +0100

    Improved types in DataLoader
    Preparing the project for generic types of data to be loaded
    Another method was added which is load_file, which enables the loading
    of a single file in the Standalone mode

--- src/cv_maker/protocols.py
+++ src/cv_maker/protocols.py
@@ -1,11 +1,19 @@
 from pathlib import Path
-from typing import Any, Generator, List, Protocol
+from typing import Generator, List, Protocol, TypeVar, Generic, Dict, Any, Tuple
+
+T = TypeVar("T", bound=Dict[str, Any], covariant=True)
 
 
-class DataLoader(Protocol):
+class DataLoader(
+    Generic[T],
+    Protocol,
+):
     def list_files(self, directory: Path) -> List[Path]: ...
-    def load_file(self, file: Path) -> dict | None: ...
-    def load_all_files_in_dir(self, path: Path) -> Generator[Any, None, None]: ...
+    def parse_file(self, file: Path) -> dict[str, str] | None: ...
+    def load_file(self, file: Path) -> Tuple[T | None, str]: ...
+    def load_all_files_in_dir(
+        self, path: Path
+    ) -> Generator[Tuple[T, str], None, None]: ...
 
 
 class BackendEngine(Protocol):
