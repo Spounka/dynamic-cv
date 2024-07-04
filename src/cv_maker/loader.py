18c22353dfdd0f8fbd70e5417b650d7b2d1eb42b
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2024-07-05 00:01:35 +0100

    Updated YamlLoader class to new changes

--- src/cv_maker/loader.py
+++ src/cv_maker/loader.py
@@ -1,11 +1,13 @@
 from typing import Generator
 from pathlib import Path
-from typing import List, Any
+from typing import List, Any, TypeVar, Generic, Tuple, Dict
 
 import yaml
 
+T = TypeVar("T", bound=Dict[str, Any], covariant=True)
 
-class YamlLoader:
+
+class YamlLoader(Generic[T]):
     def list_files(self, directory: Path) -> List[Path]:
         """Lists all yaml files in directory and returns them in a list"""
         return [
@@ -14,7 +16,7 @@ class YamlLoader:
             if file.is_file() and file.suffix in [".yml", ".yaml"]
         ]
 
-    def load_file(self, file: Path) -> dict | None:
+    def parse_file(self, file: Path) -> T | None:
         """Opens a yaml file and parses it"""
         try:
             with open(file, "r", encoding="utf-8") as stream:
@@ -23,8 +25,14 @@ class YamlLoader:
             print(err)
             return None
 
-    def load_all_files_in_dir(self, path: Path) -> Generator[Any, None, None]:
+    def load_all_files_in_dir(self, path: Path) -> Generator[Tuple[T, str], None, None]:
         for file in self.list_files(path):
-            data = self.load_file(file)
+            data, name = self.load_file(file)
             if data:
-                yield data, file.name.split(".")[0]
+                yield data, name
+
+    def load_file(self, file: Path) -> Tuple[T | None, str]:
+        data, name = (self.parse_file(file), file.name.split(".")[0])
+        if not data:
+            return None, name
+        return (data, name)
